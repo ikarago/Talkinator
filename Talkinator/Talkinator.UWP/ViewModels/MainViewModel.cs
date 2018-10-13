b5a45c1bc571adb8b0cb2e7b598c872b94a94dd8
Ikarago <ikarago@outlook.com>
2018-10-13 19:55:28 +0200

    Small work on ViewModel

--- Talkinator/Talkinator.UWP/ViewModels/MainViewModel.cs
+++ Talkinator/Talkinator.UWP/ViewModels/MainViewModel.cs
@@ -1,16 +1,123 @@
 ﻿using System;
-
+using System.Windows.Input;
 using Talkinator.UWP.Helpers;
 
 namespace Talkinator.UWP.ViewModels
 {
-    public class MainViewModel : Observable
+    public class MainViewModel : NotificationBase
     {
+        // Properties
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set { SetProperty(ref _text, value); }
+        }
+
+        private bool _isPlaying;
+        public bool IsPlaying
+        {
+            get { return _isPlaying; }
+            set { SetProperty(ref _isPlaying, value); }
+        }
+
+        private bool _isLoopOn;
+        public bool IsLoopOn
+        {
+            get { return _isLoopOn; }
+            set { SetProperty(ref _isLoopOn, value); }
+        }
+
+
+        // Speech Synth
+
+
+        // List of Voices
+
+
+
+        // Constructor
         public MainViewModel()
         {
-            // Speech Synth
-            // List with Voices
-            // Playback settings
+            // Initialize this
         }
+
+
+        // Commands
+        private ICommand _playCommand;
+        public ICommand PlayCommand
+        {
+            get
+            {
+                if (_playCommand == null)
+                {
+                    _playCommand = new RelayCommand(
+                        () =>
+                        {
+                            // #TODO
+                        });
+                }
+                return _playCommand;
+            }
+        }
+
+        private ICommand _pauseCommand;
+        public ICommand PauseCommand
+        {
+            get
+            {
+                if (_pauseCommand == null)
+                {
+                    _pauseCommand = new RelayCommand(
+                        () =>
+                        {
+                            // #TODO
+                        });
+                }
+                return _pauseCommand;
+            }
+        }
+
+        private ICommand _stopCommand;
+        public ICommand StopCommand
+        {
+            get
+            {
+                if (_stopCommand == null)
+                {
+                    _stopCommand = new RelayCommand(
+                        () =>
+                        {
+                            // # TODO
+                        });
+                }
+                return _stopCommand;
+            }
+        }
+
+        private ICommand _clearTextCommand;
+        public ICommand ClearTextCommand
+        {
+            get
+            {
+                if (_clearTextCommand == null)
+                {
+                    _clearTextCommand = new RelayCommand(
+                        () =>
+                        {
+                            ClearText();
+                        });
+                }
+                return _clearTextCommand;
+            }
+        }
+
+
+        // Methods
+        private void ClearText()
+        {
+            Text = "";
+        }
+
     }
 }
