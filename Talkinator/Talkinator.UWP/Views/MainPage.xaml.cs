71079b1ce2060b4651dea42549d2a053018f5777
Ikarago <ikarago@outlook.com>
2018-10-29 12:44:02 +0100

    Added AdaptToDevice-code

--- Talkinator/Talkinator.UWP/Views/MainPage.xaml.cs
+++ Talkinator/Talkinator.UWP/Views/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using Talkinator.UWP.ViewModels;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Talkinator.UWP.Views
@@ -15,10 +16,37 @@ namespace Talkinator.UWP.Views
         public MainPage()
         {
             InitializeComponent();
+            AdaptViewToDevice();
         }
 
 
         // Methods
+        private void AdaptViewToDevice()
+        {
+            // Check devicefamily
+            var device = Windows.ApplicationModel.Resources.Core.ResourceContext.GetForCurrentView().QualifierValues;
+            bool isDesktop = (device.ContainsKey("DeviceFamily") && device["DeviceFamily"] == "Desktop");
+            bool isXbox = (device.ContainsKey("DeviceFamily") && device["DeviceFamily"] == "Xbox");
+
+            if (isDesktop == true)
+            {
+                // Hide classic Talkinator-button
+                btnTitle.Visibility = Visibility.Collapsed;
+
+                // Show custom titlebar
+                gridTitlebar.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                // Hide custom titlebar
+                gridTitlebar.Visibility = Visibility.Collapsed;
+
+                // Show classic Talkinator-button
+                btnTitle.Visibility = Visibility.Visible;
+            }
+        }
+
+
         private async void txtTextToSay_Drop(object sender, Windows.UI.Xaml.DragEventArgs e)
         {
             if (e.DataView.Contains(StandardDataFormats.Text))
